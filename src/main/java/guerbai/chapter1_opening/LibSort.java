d4cf9d4bbe424cbb363659515dec52758a0fcd36
guerbai <guerbai8@gmail.com>
2018-05-06 10:48:00 +0800

    print method, libsort

---
+++ src/main/java/guerbai/chapter1_opening/LibSort.java
@@ -0,0 +1,31 @@
+package guerbai.chapter1_opening;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class LibSort {
+    public static void main(String[] args) throws IOException{
+        long startAt = System.currentTimeMillis();
+        String fileName = "./lot-number.txt";
+        String targetFileName = "./sorted-number2.txt";
+        FileWriter fileWriter = new FileWriter(targetFileName);
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String line;
+        ArrayList<Integer> numberArray = new ArrayList<>();
+        while ((line = br.readLine()) != null) {
+            numberArray.add(Integer.parseInt(line));
+        }
+        Collections.sort(numberArray);
+        for (int number: numberArray) {
+            fileWriter.append(Integer.toString(number)).append('\n');
+        }
+        fileWriter.close();
+        br.close();
+        long endAt = System.currentTimeMillis();
+        System.out.println("Program cost time: " + (float)(endAt-startAt)/1000 + 's');
+    }
+}
