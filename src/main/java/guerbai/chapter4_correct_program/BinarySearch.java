ece7d77e880cb4e7670496a4d9ebca4f070f31db
guerbai <guerbai8@gmail.com>
2018-05-26 23:48:52 +0800

    recursionBinarySearch

--- src/main/java/guerbai/chapter4_correct_program/BinarySearch.java
+++ src/main/java/guerbai/chapter4_correct_program/BinarySearch.java
@@ -23,14 +23,38 @@ public class BinarySearch {
         }
     }
 
+    public static int recursionBinarySearch(int[] x, int t, int l, int r) {
+        if (r<l) {
+            return -1;
+        }
+        int mid = (l+l) / 2;
+        if (x[mid] > t) {
+            return recursionBinarySearch(x, t, l, mid-1);
+        } else if (x[mid] < t) {
+            return recursionBinarySearch(x, t, mid+1, r);
+        } else {
+            return mid;
+        }
+    }
+
     public static void main(String[] args) {
         long startAt = currentTimeMillis();
 
         int[] s = {1, 3, 7, 21, 88, 155, 899, 1000, 2551, 10034, 12343, 123456, 6543211};
-        print(BinarySearch.binarySearch(s, 1000));
-        print(BinarySearch.binarySearch(s, 2657));
-        print(BinarySearch.binarySearch(s, 888888));
-        print(BinarySearch.binarySearch(s, -20));
+        print(binarySearch(s, 1000));
+        print(recursionBinarySearch(s, 1000, 0, s.length-1));
+        print(binarySearch(s, 2657));
+        print(recursionBinarySearch(s, 2657, 0, s.length-1));
+        print(binarySearch(s, 888888));
+        print(recursionBinarySearch(s, 888888, 0, s.length-1));
+        print(binarySearch(s, 6543211));
+        print(recursionBinarySearch(s, 6543211, 0, s.length-1));
+        print(binarySearch(s, -20));
+        print(recursionBinarySearch(s, -20, 0, s.length-1));
+
+        int[] s1 = {};
+        print(binarySearch(s1, 2));
+        print(recursionBinarySearch(s1, 2, 0, s1.length-1));
 
         long endAt = currentTimeMillis();
         print("Program cost time: " + (float) (endAt - startAt) / 1000 + 's');
--- src/main/java/guerbai/chapter4_correct_program/README.md
+++ src/main/java/guerbai/chapter4_correct_program/README.md
@@ -2,3 +2,7 @@
 
 # BinarySearch
 通过使用书中内容的伪码编写出正确的二分搜索程序.
+recursionBinarySearch(), 对应习题3.
+
+习题1、2的解答依书中引导留到9.3节处理。
+
